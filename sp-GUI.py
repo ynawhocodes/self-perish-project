4049438a89dbf6556a615542afcb605605ba3cca
ynawhocodes <ynawhocodes@gmail.com>
2021-05-26 14:38:54 +0900

    20210526 - 미리보기(폴더열기, 파일열기) 완료 - url이 마지막만 저장되는 오류 미해결

--- sp-GUI.py
+++ sp-GUI.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import filedialog
+from tkinter import ttk
 import os
 from datetime import datetime
 import cv2, numpy as np
@@ -39,47 +40,58 @@ def openFolder():
     global imageHeight, imageWidth, imageChannel
     imageHeight, imageWidth, imageChannel = img.shape
 
+def saveVideoUrl(i, URL):
+    url[int(i)] = URL
+    print(url[int(i)])
+
+def saveUrl(_urlEntryList):
+    for i in linkImageNum:   
+        print(_urlEntryList[int(i)].get())
+        # url[int(i)] = _urlEntryList[int(i)].get()
+        # print(url[int(i)])
+       
+
 def inputUrl():
     url_window = Toplevel(window)
     url_window.title("input URL")
     url_window.resizable(False, False)
       
-    for i in linkImageNum:
-        urlLabel = Label(url_window , text="url " + str(i)+ ": ")
-        urlLabel.grid(row=i, column=0)
-        urlEntry = Entry(url_window, width=64)
-        urlEntry.grid(row=i, column=1)
+    for i in linkImageNum:     
+        urlLabelList =  [Label(url_window , text="url")] * imageNum
+        urlLabelList[int(i)] = Label(url_window , text="url " + str(i)+ ": ")
+        urlLabelList[int(i)].grid(row=i, column=0)
+        global urlEntryList
+        urlEntryList = [Entry(url_window, width=64)] * imageNum
+        urlEntryList[int(i)] = Entry(url_window, width=64)
+        urlEntryList[int(i)].grid(row=i, column=1)
 
-        URL = urlEntry.get()
-        url[int(i)] = URL
+    save_btn = Button(url_window, text="저장", command=lambda: saveUrl(urlEntryList))
+    save_btn.grid(row=19, column=2)
+       
 
     if(chk_state.get() == True):
         video_num = Label(url_window , text="비디오 삽입된 이미지 번호")
         video_url = Label(url_window , text="비디오 url")
 
+        global video_num_entry
         video_num_entry = Entry(url_window, width=64)
         video_url_entry = Entry(url_window, width=64)
 
-        video_num.grid(row=10, column=0)
-        video_url.grid(row=11, column=0)
+        video_num.grid(row=20, column=0)
+        video_url.grid(row=21, column=0)
 
-        video_num_entry.grid(row=10, column=1)
-        video_url_entry.grid(row=11, column=1)
+        video_num_entry.grid(row=20, column=1)
+        video_url_entry.grid(row=21, column=1)
 
-        videoImageNum = video_num_entry.get()
-        videoUrl =  video_url_entry.get()
         #비디오가 있는 이미지에는 이동url이 없을 것이므로 같은 리스트를 이용
-        url[int(videoImageNum)] = videoUrl
+
+        save_btn = Button(url_window, text="저장", command=lambda: saveVideoUrl(video_num_entry.get(), video_url_entry.get()))
+        save_btn.grid(row=21, column=2)
    
-    save_btn = Button(url_window, text="저장")
-    save_btn.grid(row=20, column=0, columnspan=2)
-
-
 
 
 # 이미지 개수
 parameter = str(datetime.now().year) + str(datetime.now().month) + str(datetime.now().day) + str(datetime.now().hour) + str(datetime.now().minute)
-
 
 def setPosition():
     global linkImageNum
@@ -103,11 +115,15 @@ def saveFile():
 
 def savePC():
     outfile = open(filePath + "\\sp.html", "w")
-    # 전체 틀 코드도 작성해야함
-    outfile.write("<style>\n    .n-detail-special {background-color:#" + bgColor + ";}")
-    style(top, marginLeft, width, height, "px")
-    blockCode()
+    # 전체 틀
+    outfile.write("<!DOCTYPE html>\n<html lang=\"ko\">\n<head>\n    <link type=\"text/css\" rel=\"stylesheet\" href=\"https://image.msscdn.net/ui/utility/event/css/common.css?202005181853\">\n    <style>* {margin:0;padding:0;} .link{background-color:rgba(212,5,5,.5)}</style>\n    <title>special issue</title>\n</head>\n<body>\n")
+    outfile.write("<div class=\"n-detail-special\">\n")
 
+    outfile.write("<style>\n    .n-detail-special {background-color:#" + bg_color_entry.get() + ";}\n")
+    style(top, marginLeft, width, height, "px", outfile)
+    blockCode(outfile)
+
+    outfile.write("</div>\n</body>\n</html>")
     outfile.close()
 
 def saveMB():
@@ -116,18 +132,29 @@ def saveMB():
     pcToM(marginLeft, top)
     pcToM(width, height)
 
-    # mobile
-    # 전체 틀 코드도 작성해야함
+    # 전체 틀
+    outfile.write("<!DOCTYPE html>\n<html lang=\"ko\">\n<head>\n    <link type=\"text/css\" rel=\"stylesheet\" href=\"https://image.msscdn.net/ui/utility/event/css/m_common.css?202005141625\">\n    <style>* {margin:0;padding:0;} img{max-width:100%; vertical-align: top; border:0;} .link{background-color:rgba(212,5,5,.5)}</style>\n    <title>special issue</title>\n</head>\n<body>\n")
+    outfile.write("<div class=\"n-detail-special\">\n")
+    
     outfile.write("<style>\n")
-    style(top, marginLeft, width, height, "%")
-    blockCode()
+    style(top, marginLeft, width, height, "%", outfile)
+    blockCode(outfile)
 
+    outfile.write("</div>\n</body>\n</html>")
     outfile.close()
-        
+
+def preview():
+    os.startfile(filePath)
+
+def previewPC():
+    os.startfile(filePath + "\\sp.html")
+
+def previewMB():
+    os.startfile(filePath + "\\sp-m.html")
 
 window = Tk()
 window.title("special issue")
-window.geometry("320x118+1200+500")
+window.geometry("300x150+1200+500")
 window.resizable(False, False)
 
 
@@ -136,12 +163,17 @@ folder_path = Label(window , text="이미지 경로")
 link_img_num = Label(window , text="링크있는 이미지 번호")
 bg_color = Label(window, text="배경색")
 cyberduck_path = Label(window, text="CyberDuck 폴더명")
+preview_state=BooleanVar()
+preview_state.set(False)
+preview_check = Label(window , text="미리보기")
 
 # label grid
 folder_path.grid(row=0, column=0)
 link_img_num.grid(row=1, column=0)
 bg_color.grid(row=2, column=0)
 cyberduck_path.grid(row=3, column=0)
+preview_check.grid(row=6, column=0)
+
 
 # btn
 folder_path_btn = Button(window, text="찾아보기", command=openFolder, width=20)
@@ -149,44 +181,46 @@ link_img_num_entry = Entry(window)
 bg_color_entry = Entry(window)
 cyberduck_path_entry = Entry(window)
 
+preview_btn = Button(window, text="폴더열기", command=preview, width=6)
+previewPC_btn = Button(window, text="PC", command=previewPC, width=6)
+previewMB_btn = Button(window, text="MB", command=previewMB, width=6)
+   
+
 # btn grid
 folder_path_btn.grid(row=0, column=1, columnspan=6)
 link_img_num_entry.grid(row=1, column=1, columnspan=6)
 bg_color_entry.grid(row=2, column=1, columnspan=6)
 cyberduck_path_entry.grid(row=3, column=1, columnspan=6)
+preview_btn.grid(row=6, column=1)
+previewPC_btn.grid(row=6, column=2)
+previewMB_btn.grid(row=6, column=3)
 
 # url setting
 chk_state=BooleanVar()
 chk_state.set(False)
 video_check = Checkbutton(window, text="video", var=chk_state)
-url_position_btn = Button(window, text="url위치", command=setPosition)
-url_btn = Button(window, text="url입력", command=inputUrl)
-preview_btn = Button(window, text="미리보기")
-save_btn = Button(window, text="저장", command=saveFile)
+url_position_btn = Button(window, text="url위치", command=setPosition, width=6)
+url_btn = Button(window, text="url입력", command=inputUrl, width=6)
+save_btn = Button(window, text="저장", command=saveFile, width=6)
+
+
 
 # url grid
 video_check.grid(row=5, column=0)
 url_position_btn.grid(row=5, column=1)
 url_btn.grid(row=5, column=2)
-preview_btn.grid(row=5, column=3)
-save_btn.grid(row=5, column=4)
-
+save_btn.grid(row=5, column=3)
 
 
 # style 출력
-def style(topList, marginLeftList, widthList, heightList, unit): 
+def style(topList, marginLeftList, widthList, heightList, unit, outfile): 
     for i in linkImageNum:
-        outfile.write("    .n-detail-special .block" + str(i) + " .link {top:" + topList[int(i)] + unit + "; margin-left:" + marginLeftList[int(i)] + unit + "; width: " + widthList[int(i)] + unit  + "; height: " + heightList[int(i)] + unit + ";}")        
+        outfile.write("    .n-detail-special .block" + str(i) + " .link {top:" + topList[int(i)] + unit + "; margin-left:" + marginLeftList[int(i)] + unit + "; width: " + widthList[int(i)] + unit  + "; height: " + heightList[int(i)] + unit + ";}\n")        
         
-    if(isVideo == 'y'):
-        outfile.write("    .video-wrap {position:absolute; left:0; top:0%; width:92%; margin-left:4%;}\n    .n-detail-special .video-wrap .video-content {position:relative; padding-top:56.25%;}\n    .n-detail-special .video-wrap .video-content iframe {position:absolute; left:0; top:0; width:100%; height:100%;}")
+    if(chk_state.get() == True):
+        outfile.write("    .video-wrap {position:absolute; left:0; top:0%; width:92%; margin-left:4%;}\n    .n-detail-special .video-wrap .video-content {position:relative; padding-top:56.25%;}\n    .n-detail-special .video-wrap .video-content iframe {position:absolute; left:0; top:0; width:100%; height:100%;}\n")
 
     outfile.write("</style>\n")
-
-global bgColor
-bgColor = bg_color_entry.get()
-global folderName
-folderName = cyberduck_path_entry.get()
 
 
 # pc to mb
@@ -195,24 +229,23 @@ def pcToM(pcWidthStyle, pcHeightStyle):
         pcWidthStyle[int(i)] = format(int(pcWidthStyle[int(i)]) / 10.8, ".2f")
         pcHeightStyle[int(i)] = format(int(pcHeightStyle[int(i)]) * 100 / imageHeight, ".2f")
 
-
 # 결과 코드
-def blockCode():        
+def blockCode(outfile):        
     for i in range(1, imageNum):
         if(chk_state.get() == True):
             if str(i) in linkImageNum:                   
-                outfile.write("<div class=\"block block" + str(i) + "\"><a href=\"" + url[i] + "\" class=\"link\">링크</a><img src=\"https://image.msscdn.net/musinsaUI/specialissue/" + folderName + "/img_" + str(i)+".jpg?" + parameter + "\" alt=\"\"></div>")             
+                outfile.write("<div class=\"block block" + str(i) + "\"><a href=\"" + url[i] + "\" class=\"link\">link</a><img src=\"https://image.msscdn.net/musinsaUI/specialissue/" + cyberduck_path_entry.get() + "/img_" + str(i)+".jpg?" + parameter + "\" alt=\"\"></div>\n")             
             else:
-                if(str(i) != videoImageNum):
-                    outfile.write("<div class=\"block block" + str(i) + "\"><img src=\"https://image.msscdn.net/musinsaUI/specialissue/" + folderName + "/img_" + str(i) + ".jpg?" + parameter + "\" alt=\"\"></div>")
+                if(str(i) != video_num_entry.get()):
+                    outfile.write("<div class=\"block block" + str(i) + "\"><img src=\"https://image.msscdn.net/musinsaUI/specialissue/" + cyberduck_path_entry.get() + "/img_" + str(i) + ".jpg?" + parameter + "\" alt=\"\"></div>\n")
                 else:
-                    outfile.write("<div class=\"block block" + videoImageNum + "\"><img src=\"https://image.msscdn.net/musinsaUI/specialissue/" + folderName + "/img_" + str(i)+".jpg?" + parameter + "\" alt=\"\">\n        <div class=\"video-wrap\"><div class=\"video-content\">\n            <iframe src=\"https://www.youtube.com/embed/" + videoUrl + " frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>\n        </div>\n    </div>\n</div>")                         
+                    outfile.write("<div class=\"block block" + video_num_entry.get() + "\"><img src=\"https://image.msscdn.net/musinsaUI/specialissue/" + cyberduck_path_entry.get() + "/img_" + str(i)+".jpg?" + parameter + "\" alt=\"\">\n        <div class=\"video-wrap\"><div class=\"video-content\">\n            <iframe src=\"https://www.youtube.com/embed/" + video_url_entry.get() + " frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>\n        </div>\n    </div>\n</div>\n")                         
 
         else:
             if str(i) in linkImageNum:
-                outfile.write("<div class=\"block block" + str(i) + "\"><a href=\"" + url[i] + "\" class=\"link\">링크</a><img src=\"https://image.msscdn.net/musinsaUI/specialissue/" + folderName + "/img_" + str(i)+".jpg?" + parameter + "\" alt=\"\"></div>")
+                outfile.write("<div class=\"block block" + str(i) + "\"><a href=\"" + url[i] + "\" class=\"link\">link</a><img src=\"https://image.msscdn.net/musinsaUI/specialissue/" + cyberduck_path_entry.get() + "/img_" + str(i)+".jpg?" + parameter + "\" alt=\"\"></div>\n")
             else:
-                outfile.write("<div class=\"block block" + str(i) + "\"><img src=\"https://image.msscdn.net/musinsaUI/specialissue/" + folderName + "/img_" + str(i) + ".jpg?" + parameter + "\" alt=\"\"></div>")                     
+                outfile.write("<div class=\"block block" + str(i) + "\"><img src=\"https://image.msscdn.net/musinsaUI/specialissue/" + cyberduck_path_entry.get() + "/img_" + str(i) + ".jpg?" + parameter + "\" alt=\"\"></div>\n")                     
 
 
 window.mainloop()
