0988b83032b8556d647657bac606377ba068d02d
양나원/프론트엔드개발팀 <nawon.yang@musinsa.com>
2021-08-23 10:54:05 +0900

    bug #18

--- special-issue/build-up/pre-step/step-test/OpenCV/ROItest_ROIselect_findPosition.py
+++ special-issue/build-up/pre-step/step-test/OpenCV/ROItest_ROIselect_findPosition.py
@@ -1,10 +1,12 @@
 # selectROI로 관심영역 지정 및 표시, 저장 (roi_select_img.py)
-
 import cv2, numpy as np
 import sys
 
-img = cv2.imread('/Users/yang/project/self-perish-project/special-issue/pre-step/step-test/OpenCV/img_05.jpg')
+# mac test
+# img = cv2.imread('/Users/yang/project/self-perish-project/special-issue/pre-step/step-test/OpenCV/img_05.jpg')
 
+# window test
+img = cv2.imread('C:\\Users\\nw139\OneDrive\\바탕 화면\\images\\image_1.jpg')
 if img is None:
     print('Image load failed')
     sys.exit()
